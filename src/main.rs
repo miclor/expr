2435b9e05e27b14eb248e19e0a72feb3d8768f63
Michael Loris <m.loris@comventure.de>
2020-08-10 16:52:11 +0200

    working lexer

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 
 use regex::Regex;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum TokenType {
     OpenParen,
     CloseParen,
@@ -9,17 +9,20 @@ enum TokenType {
     Whitespace
 }
 
-#[derive(Debug)]
+
+#[derive(Debug, PartialEq)]
 struct Token<'a> {
     value: &'a str,
     token_type: TokenType,
 }
+
 
 impl<'a> Token<'a> {
    pub fn new (val: &'a str, t_type: TokenType) -> Token<'a> {
         Token { value: val, token_type: t_type }
     }
 }
+
 
 #[derive(Debug)]
 struct Lexer<'a> {
@@ -29,91 +32,121 @@ struct Lexer<'a> {
 }
 
 
-
 impl<'a> Lexer<'a> {
     pub fn new(input_string: &'a str) -> Lexer<'a> {
         Lexer { tokens: Vec::new(), input_string: input_string, state: 0}
     }
 
     fn lex(&mut self) {
-
         while self.state < self.input_string.len() {
-
-            println!("checking whitespace, {:?}", self.state);
-            self.tokens.push(self.match_whitespace().unwrap());
-            println!("checking open paren, {:?}", self.state);
-            self.tokens.push(self.match_open_paren().unwrap());
-            println!("checking close paren: {:?}", self.state);
-            self.tokens.push(self.match_close_paren().unwrap());
-            println!("checking atom: {:?}", self.state);
-            self.tokens.push(self.match_atom().unwrap());
-
+            self.match_whitespace();
+            self.match_open_paren();
+            self.match_close_paren();
+            self.match_atom();
         }
-
-        println!("state: {:?}", self.state);
-
     }
 
-    fn match_open_paren(&'a mut self) -> Option<Token<'a>> {
+    fn match_open_paren(& mut self)  {
         let re_open_paren = Regex::new("^\\(").unwrap();
         let mat = re_open_paren.find(&self.input_string[self.state.. self.input_string.len()]);
 
         if mat.is_some() {
-            println!("  found match for open paren at {:?}", &self.input_string[self.state..]);
             let length = mat.unwrap().end();
             self.state += length;
-            // return length;
-            return Some(Token { value: "(", token_type: TokenType::OpenParen });
+            self.tokens.push(Token { value: "(", token_type: TokenType::OpenParen });
         }
-        return None;
+
     }
 
-    fn match_close_paren(&mut self) -> Option<Token> {
+    fn match_close_paren(&mut self) {
         let re_close_paren = Regex::new("^\\)").unwrap();
         let mat = re_close_paren.find(&self.input_string[self.state.. self.input_string.len()]);
 
         if mat.is_some() {
-            println!("  found match for close paren at {:?}", &self.input_string[self.state..]);
             let length = mat.unwrap().end();
             self.state += length;
-            return Some(Token { value: ")", token_type: TokenType::CloseParen });
+            self.tokens.push(Token { value: ")", token_type: TokenType::CloseParen });
         }
-        return None;
+
     }
 
-    fn match_atom(&mut self) -> Option<Token> {
+    fn match_atom(&mut self) {
         let re_atom = Regex::new("^\\w+").unwrap();
         let mat = re_atom.find(&self.input_string[self.state.. self.input_string.len()]);
 
         if mat.is_some() {
-            println!("  found match for atom at {:?}", &self.input_string[self.state..]);
             let length = mat.unwrap().end();
             self.state += length;
-            return Some(Token { value: mat.unwrap().as_str(), token_type: TokenType::Atom });
+            self.tokens.push(Token { value: mat.unwrap().as_str(), token_type: TokenType::Atom });
         }
-        return None;
+
     }
 
-    fn match_whitespace(&mut self) -> Option<Token> {
+    fn match_whitespace(&mut self) {
         let re_atom = Regex::new(r"^\s").unwrap();
         let mat = re_atom.find(&self.input_string[self.state.. self.input_string.len()]);
 
         if mat.is_some() {
-            println!("  found match for whitespace at {:?}", &self.input_string[self.state..]);
             let length = mat.unwrap().end();
             self.state += length;
-            return Some(Token { value: mat.unwrap().as_str(), token_type: TokenType::Whitespace });
+            //self.tokens.push(Token { value: mat.unwrap().as_str(), token_type: TokenType::Whitespace });
         }
-        println!("  did not find match for whitespace at{:?}", self.state);
-        return None;
+    }
+
+    fn match_token(&mut self, reg_ex: &str, token_type: TokenType) {
+        let re_atom = Regex::new(reg_ex).unwrap();
+        let mat = re_atom.find(&self.input_string[self.state.. self.input_string.len()]);
+
+        if mat.is_some() {
+            let length = mat.unwrap().end();
+            self.state += length;
+            self.tokens.push(Token { value: mat.unwrap().as_str(), token_type: token_type });
+        }
     }
 }
 
+#[derive(Debug)]
+struct Parser<'a> {
+    tokens: &'a Vec<Token<'a>>,
+    state: usize,
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(tokens: &'a std::vec::Vec<Token<'a>>) -> Parser<'a> {
+        Parser { tokens: &tokens, state: 0}
+    }
+
+    pub fn parse_expr(&mut self) {
+        
+        if *&self.tokens[self.state].token_type == TokenType::Atom {
+            println!("found an atom: {:?}", self.state);
+            self.state += 1;
+        } else if *&self.tokens[self.state].token_type == TokenType::OpenParen {
+            println!("found an open paren: {:?}", self.state);
+            self.state += 1;
+        }
+
+        if *&self.tokens[self.state].token_type == TokenType::CloseParen {
+            println!("found an close paren: {:?}", self.state);
+            self.state += 1;
+        }
+
+    }
+
+}
 
 fn main() {
-    let mut l = Lexer::new("( test )");
+    let mut l = Lexer::new(" test ");
     l.lex();
-    println!("yay")
+
+    for i in &l.tokens {
+        println!("Token: {:?}", i);
+    }
+
+    let mut p = Parser::new(&l.tokens);
+
+    p.parse_expr();
+
 }
 
 
