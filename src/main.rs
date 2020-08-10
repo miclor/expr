6ff6452fa6a185a7403ab37eb5cf36490eb9d013
Michael Loris <m.loris@comventure.de>
2020-08-10 14:45:53 +0200

    updated lexer

--- src/main.rs
+++ src/main.rs
@@ -20,26 +20,46 @@ struct Lexer<'a> {
     tokens: Vec<Token<'a>>,
 }
 
+fn match_open_paren(input_string: &str) -> Result<usize, &str>{
+    let re_open_paren = Regex::new(r"^[(]").unwrap();
+    let mat = re_open_paren.find(input_string);
+
+    if mat.is_some() {
+        let length = mat.unwrap().end();
+        return Ok(length);
+    }
+    return Err("No match");
+}
+
+fn match_close_paren() {
+
+}
+
+fn match_atom() {
+
+}
+
+
 impl<'a> Lexer<'a> {
     pub fn new(input_string: &'a str) -> Lexer<'a> {
         Lexer { tokens: Vec::new(), input_string: input_string}
     }
 
     fn lex(&self, source_text: &'a str)  {
-        let re_open_paren = Regex::new(r"[(]{1}").unwrap();
-        let re_close_paren = Regex::new(r"[)]{1}").unwrap();
-        let re_atom = Regex::new(r"[0-9A-za-z].*").unwrap();
+        // let re_open_paren = Regex::new(r"^[(]").unwrap();
+        // let re_close_paren = Regex::new(r"&^[)]").unwrap();
+        // let re_atom = Regex::new(r"^[0-9A-za-z].*").unwrap();
+        // let re_whitespace = Regex::new(r"^[\s]").unwrap();
 
-        let n = 0;
+        let mut n = 0;
 
-        while (n < source_text.length()) {
+        while n < source_text.len() {
 
-            n =
+
+
         }
 
-        assert!(re_open_paren.is_match("("));
-        assert!(re_close_paren.is_match(")"));
-        assert!(re_atom.is_match("Atom123"));
+
     }
 }
 
